d0ea87c488d410b89e3d2b2560c930c4373eedf0
ZhouBing <5118757+ZhouBing007@user.noreply.gitee.com>
2020-03-13 16:06:55 +0800

    init project

---
+++ yx-admin/src/main/java/com/yx/yxadmin/YxAdminApplication.java
@@ -0,0 +1,13 @@
+package com.yx.yxadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YxAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YxAdminApplication.class, args);
+    }
+
+}
